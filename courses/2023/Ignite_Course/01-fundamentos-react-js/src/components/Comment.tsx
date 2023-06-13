ab3ef9cab936da636dd7821ddbedfca519fbecc6
italo.amaral <italorockamaral@gmail.com>
2023-06-13 16:37:52 -0300

    feat: added TS

--- courses/2023/Ignite_Course/01-fundamentos-react-js/src/components/Comment.tsx
+++ courses/2023/Ignite_Course/01-fundamentos-react-js/src/components/Comment.tsx
@@ -1,11 +1,25 @@
+import React, { useState } from "react";
 import styles from "./Comment.module.css";
 
-import { ThumbsUp, Trash } from "@phosphor-icons/react";
+import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "./Avatar";
 
-export function Comment({ content, onDeleteComment }) {
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
   }
 
   return (
@@ -31,9 +45,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
--- courses/2023/Ignite_Course/01-fundamentos-react-js/src/components/Sidebar.tsx
+++ courses/2023/Ignite_Course/01-fundamentos-react-js/src/components/Sidebar.tsx
@@ -1,20 +1,20 @@
 import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
-import { PencilLine } from '@phosphor-icons/react'
+import { PencilLine } from "phosphor-react";
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <img
-        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
         className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
 
       <div className={styles.profile}>
-        <Avatar hasBorder={true} src="https://github.com/italoRAmaral.png" />
+        <Avatar src="https://github.com/italoRAmaral.png" />
 
         <strong>Italo Amaral</strong>
-        <span>Software Developer</span>
+        <span>Web Developer</span>
       </div>
 
       <footer>
