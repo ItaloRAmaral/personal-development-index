84578925caccd0453b40249ae925522abd095efe
italo.amaral <italo.amaral@dynamox.net>
2023-10-10 08:34:28 -0300

    feat: created prisma factories for testing e2e

--- courses/2023/Ignite_Course/Nodejs_Module/05-nest-js/test/factories/make-question.ts
+++ courses/2023/Ignite_Course/Nodejs_Module/05-nest-js/test/factories/make-question.ts
@@ -5,7 +5,13 @@ import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaQuestionMapper } from '@/infra/database/prisma/mappers/prisma-question-mapper'
+import { Injectable } from '@nestjs/common'
 
+/*
+  Criando uma factory para a entidade Question, que será utilizada para criar testes dos casos de uso do domínio
+*/
 export function makeQuestion(
   override: Partial<QuestionProps> = {},
   id?: UniqueEntityID,
@@ -21,4 +27,24 @@ export function makeQuestion(
   )
 
   return question
+}
+
+/*
+  Criando uma factory para a entidade Question do Prisma, que será utilizada para criar testes e2e (controllers)
+*/
+@Injectable()
+export class QuestionFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestion(
+    data: Partial<QuestionProps> = {},
+  ): Promise<Question> {
+    const question = makeQuestion(data)
+
+    await this.prisma.question.create({
+      data: PrismaQuestionMapper.toPrisma(question),
+    })
+
+    return question
+  }
 }
